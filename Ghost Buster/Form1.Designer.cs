38eb44bd5a5a567ad106b03229faf09b9e1dde3d
Laura-Foxey <roura_sasunaru@hotmail.com>
2022-10-10 23:37:56 +0300

    Add project files.

---
+++ Ghost Buster/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ghost_Buster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtKills = new System.Windows.Forms.Label();
+            this.txtHP = new System.Windows.Forms.Label();
+            this.HPBar = new System.Windows.Forms.ProgressBar();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("SimSun-ExtB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.Location = new System.Drawing.Point(12, 9);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(86, 19);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            this.txtAmmo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKills
+            // 
+            this.txtKills.AutoSize = true;
+            this.txtKills.Font = new System.Drawing.Font("SimSun-ExtB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKills.Location = new System.Drawing.Point(234, 9);
+            this.txtKills.Name = "txtKills";
+            this.txtKills.Size = new System.Drawing.Size(97, 19);
+            this.txtKills.TabIndex = 0;
+            this.txtKills.Text = "Kills: 0";
+            this.txtKills.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHP
+            // 
+            this.txtHP.AutoSize = true;
+            this.txtHP.Font = new System.Drawing.Font("SimSun-ExtB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHP.Location = new System.Drawing.Point(481, 9);
+            this.txtHP.Name = "txtHP";
+            this.txtHP.Size = new System.Drawing.Size(42, 19);
+            this.txtHP.TabIndex = 0;
+            this.txtHP.Text = "HP:";
+            this.txtHP.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // HPBar
+            // 
+            this.HPBar.Location = new System.Drawing.Point(529, 5);
+            this.HPBar.Name = "HPBar";
+            this.HPBar.Size = new System.Drawing.Size(177, 23);
+            this.HPBar.TabIndex = 1;
+            this.HPBar.Value = 100;
+            // 
+            // player
+            // 
+            this.player.Image = global::Ghost_Buster.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(287, 221);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.HPBar);
+            this.Controls.Add(this.txtHP);
+            this.Controls.Add(this.txtKills);
+            this.Controls.Add(this.txtAmmo);
+            this.Name = "Form1";
+            this.Text = "Ghost Buster";
+            this.Load += new System.EventHandler(this.w);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtKills;
+        private System.Windows.Forms.Label txtHP;
+        private System.Windows.Forms.ProgressBar HPBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Ghost Buster/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ghost_Buster
+{
+    public partial class Form1 : Form
+    {
+        bool goUp, goDown, goLeft, goRight, gameOver = false;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int enemySpeed = 2;
+        int kills = 0;
+        Random random = new Random();
+
+        List<PictureBox> enemyList = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 0)
+            {
+                HPBar.Value = playerHealth;
+            }
+            else
+            {
+                gameOver = true;
+            }
+
+            txtAmmo.Text = "Ammo: " + ammo;
+            txtKills.Text = "Kills: " + kills;
+
+            if (goUp == true && player.Top > 0)
+            {
+                player.Top -= speed;
+            }
+            if (goDown == true && player.Top + player.Height < ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+            if (goRight == true && player.Left + player.Width < ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                Shoot(facing);
+            }
+        }
+
+        private void w(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+        }
+
+        private void Shoot(string dir)
+        {
+
+        }
+
+        private void SpawnEnemy()
+        {
+
+        }
+
+        private void Reset()
+        {
+
+        }
+    }
+}
